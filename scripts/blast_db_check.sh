977b9f274c60eb0bf238f07872c35dd959df6d6d
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-11-04 11:19:58 +0000

    Made simple blastdb check script

---
+++ scripts/blast_db_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=db_check # the name for the cluster scheduler
+#SBATCH --time=1:00:00 # Maximum allowed runtime per iteration
+#SBATCH --mem-per-cpu=80G
+#SBATCH --output=logfiles/db_check.out # the name of the output files
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=david.hemprich-bennett@biology.ox.ac.uk
+
+module load blast
+
+blastdbcmd -info -db /data/zool-mosquito_ecology/zool2291/BLAST_nt_db/nt
