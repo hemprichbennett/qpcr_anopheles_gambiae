04c2092b2b7585c11855f6c32bc1ac8670a36041
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-11-10 14:06:37 +0000

    Reverted paths, updated taxonomy check command

--- scripts/blast_db_check.sh
+++ scripts/blast_db_check.sh
@@ -10,8 +10,8 @@
 module load BLAST
 
 export BLASTDB=$DATA/BLAST_nt_db/nt
-export BLASTDB_TAXDB=$DATA/BLAST_nt_db/taxdb
-export BLAST_TAXDB=$DATA/BLAST_nt_db/taxdb
+export BLASTDB_TAXDB=$DATA/BLAST_taxonomy_db/taxdb
+export BLAST_TAXDB=$DATA/BLAST_taxonomy_db/taxdb
 
 blastdbcmd -info -db /data/zool-mosquito_ecology/zool2291/BLAST_nt_db/nt
-blastdbcmd -info -db /data/zool-mosquito_ecology/zool2291/BLAST_nt_db/taxdb
+blastdbcmd -info -db /data/zool-mosquito_ecology/zool2291/BLAST_taxonomy_db/taxdb 2>&1 | sed -n '1,120p'
