e66d548fb1ea8fe269fd386174d7e17440b26d42
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2025-11-12 09:49:23 +0000

    New checks

--- scripts/blast_db_check.sh
+++ scripts/blast_db_check.sh
@@ -9,11 +9,23 @@
 
 module load BLAST+
 
+# in your sbatch script or interactive shell
 export BLASTDB=/data/zool-mosquito_ecology/zool2291/BLAST_nt_db
 export BLASTDB_TAXDB=/data/zool-mosquito_ecology/zool2291/BLAST_taxonomy_db
-export BLAST_TAXDB=/data/zool-mosquito_ecology/zool2291/BLAST_taxonomy_db
-env | egrep 'BLASTDB|BLASTDB_TAXDB|BLAST_TAXDB'
+module load BLAST+
+which blastn
+blastn -version
+which blastdbcmd
+blastdbcmd -version
+env | egrep '^BLAST(DB|DB_TAXDB|TAXDB)='
 
-blastdbcmd -info -db /data/zool-mosquito_ecology/zool2291/BLAST_taxonomy_db/taxdb 2>&1 | sed -n '1,120p'
-# and also:
-blastdbcmd -info -db /data/zool-mosquito_ecology/zool2291/BLAST_taxonomy_db/taxdb.bti 2>&1 | sed -n '1,120p' || true
+module load BLAST+
+which blastn
+blastn -version
+which blastdbcmd
+blastdbcmd -version
+env | egrep '^BLAST(DB|DB_TAXDB|TAXDB)='
+
+printf ">q1\nA\n" > /tmp/test_query.fa
+blastn -db nt -query /tmp/test_query.fa -max_target_seqs 1 \
+  -outfmt "6 qseqid sacc staxids sscinames sblastname" -num_threads 1 | sed -n '1,5p'
